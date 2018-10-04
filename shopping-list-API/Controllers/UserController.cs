88fbf48eef18bd311bb6cd83464364e6b456ce76
Chris <clc523@gmail.com>
2018-10-04 07:22:28 -0600

    updated configuration and testing for aws web server. Added POST route for Users.

---
+++ shopping-list-API/Controllers/UserController.cs
@@ -0,0 +1,72 @@
+﻿using shopping_list_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace shopping_list_API.Controllers
+{
+    public class UserController : ApiController
+    {
+        private Context _context = null;
+
+        public UserController()
+        {
+            _context = new Context();
+        }
+
+        public IEnumerable<User> Get()
+        {
+            var users = _context.Users.ToList();
+            return users;
+        }
+
+        public User Get(int id)
+        {
+            return null;
+        }
+
+        public void Post(User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+        }
+
+        public string[] Put()
+        {
+            var appConfig = ConfigurationManager.AppSettings;
+            string dbname = appConfig["RDS_DB_NAME"];
+            if (string.IsNullOrEmpty(dbname)) return null;
+
+            string username = appConfig["RDS_USERNAME"];
+            string password = appConfig["RDS_PASSWORD"];
+            string hostname = appConfig["RDS_HOSTNAME"];
+            string port = appConfig["RDS_PORT"];
+
+            return new string[5] { dbname, username, password, hostname, port };
+        }
+
+        public string Delete()
+        {
+            return "this is a test";
+        }
+
+        //private bool _disposed = false;
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (_disposed)
+        //        return;
+
+        //    if (disposing)
+        //    {
+        //        _context.Dispose();
+        //    }
+
+        //    _disposed = true;
+
+        //    base.Dispose(disposing);
+        //}
+    }
+}
---
+++ shopping-list-API/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace shopping_list_API.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<shopping_list_API.Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(shopping_list_API.Context context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
